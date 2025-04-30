3f1eb40b157bbb813a71ae430e4200befa5b643f
Johnny Michael Alves Faria <johnny.faria1992@gmail.com>
2025-04-30 02:18:27 +0000

    Fix: usar HashRouter e corrigir import Login

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Login from "./Login";
-import Home from "./Home";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import Login from "./components/login";
+import Home from "./components/home";
 
 function App() {
   return (
